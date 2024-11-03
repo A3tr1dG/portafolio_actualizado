72e5cd35dcc04270406176fb6116ea49733b6f39
A3tr1dG <astridgonzalezr2017@gmail.com>
2024-11-02 22:53:52 -0600

    update

--- src/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loader = document.getElementById("loader-wrapper");
-    if (loader) {
-      loader.style.display = "none";
-    }
-  });
