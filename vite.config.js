4114bab185ea6123e454189d2e344f02652847a5
A3tr1dG <astridgonzalezr2017@gmail.com>
2024-11-02 22:05:47 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import * as glob from "glob";
+import path, { resolve } from "node:path";
 
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    minify: true,   
-  },
-});
+const getHtmlEntries = ()=>{
+    return Object.fromEntries(
+        [
+            ...glob.sync('./**/*.html', { ignore:['./dist/**','./node_modules/**']}).map(file=>[
+                file.slice(0, file.length - path.extname(file).length),
+                resolve(__dirname, file)
+            ])
+        ]
+    )
+}
+
+export default defineConfig(
+    {
+        appType: 'mpa',
+        base: "/portafolio_astrid_gonzalez",
+        build: {
+            rollupOptions: {
+                input: getHtmlEntries()
+            }
+        },
+        plugins: [
+            htmlPurge({}),
+        ]
+    }
+);
