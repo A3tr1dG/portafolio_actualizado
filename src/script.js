faa5e8619277f1a2f60a547e7fb6b7c5c569732f
A3tr1dG <astridgonzalezr2017@gmail.com>
2024-11-02 22:59:09 -0600

    final update

---
+++ src/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loader = document.getElementById("loader-wrapper");
+    if (loader) {
+      loader.style.display = "none";
+    }
+  });
